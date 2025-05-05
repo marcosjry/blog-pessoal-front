f076359f2994116ee1ebe6f5bf6b504da456c754
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-05-05 14:14:39 -0300

    feat: adicionado mudanças na estrutura base do projeto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './auth/auth.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 export const appConfig: ApplicationConfig = {
@@ -12,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([authInterceptor])
-    )
+    ),
+    provideAnimations(),
+    MatDialogModule
   ]
 };
+
